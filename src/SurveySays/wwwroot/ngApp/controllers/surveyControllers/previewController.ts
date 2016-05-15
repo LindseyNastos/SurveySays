b52cc938cd0b721e1832d155fb4aac5a56e61d8b
Lindsey <lindseynastos@gmail.com>
2016-05-14 23:41:41 -0700

    Playing with addHtml() for answers on preview page

--- src/SurveySays/wwwroot/ngApp/controllers/surveyControllers/previewController.ts
+++ src/SurveySays/wwwroot/ngApp/controllers/surveyControllers/previewController.ts
@@ -2,13 +2,56 @@
 
     export class PreviewController {
         public survey: SurveySays.Models.ISurvey;
-        public questions: SurveySays.Models.IQuestion[];
+        public questions: any = [];
         constructor(private surveyService: SurveySays.Services.SurveyService, $stateParams: ng.ui.IStateParamsService) {
             surveyService.getFullSurvey($stateParams['id']).then((data) => {
                 this.survey = data.survey;
                 this.questions = data.questions;
+                for (let q of this.questions) {
+                    this.addHtml(q);
+                };
             });
         }
+
+        public addHtml(question: SurveySays.Models.IQuestion) {
+            //let htmlAnswers = [];
+            debugger;
+            if (question.questionType.type == "TextBox") {
+                //htmlAnswers.push('<input class="form-control" type="text" name="{{question.id}}" /><br />');
+                let x = <HTMLElement>document.getElementById((question.id).toString());
+                x.innerHTML = '<input class="form-control" type="text" name="{{question.id}}" /><br />';
+            }
+            else if (question.questionType.type == "Multiple Choice") {
+                //for (let a of question.answerOptions) {
+                //    htmlAnswers.push('<div ng-repeat="answer in question.answerOptions"><input type="radio" name="{{question.id}}" value="{{answer.id}}" />{{answer.opt}}<br /></div>');
+                //}
+                for (let a of question.answerOptions) {
+                    let x = <HTMLElement>document.getElementById((question.id).toString());
+                    x.innerHTML = '<div ng-repeat="answer in question.answerOptions"><input type="radio" name="{{question.id}}" value="{{answer.id}}" />{{answer.opt}}<br /></div>';
+                }
+            }
+            else if (question.questionType.type == "Ranking") {
+                //htmlAnswers.push('<div ng-repeat="answer in question.answerOptions"><input type="number" name="{{question.id}}" />{{answer.opt}}<br /></div>');
+                let x = <HTMLElement>document.getElementById((question.id).toString());
+                x.innerHTML = '<div ng-repeat="answer in question.answerOptions"><input type="number" name="{{question.id}}" />{{answer.opt}}<br /></div>';
+
+            }
+            else if (question.questionType.type == "Dropdown List") {
+                //htmlAnswers.push('<select class="form-control"><option value="{{answer.id}}" ng-repeat="answer in question.answerOptions">{{answer.opt}}</option></select>');
+                let x = <HTMLElement>document.getElementById((question.id).toString());
+                x.innerHTML = '<select class="form-control"><option value="{{answer.id}}" ng-repeat="answer in question.answerOptions">{{answer.opt}}</option></select>';
+            }
+            else if (question.questionType.type == "Matrix Rating") {
+                //htmlAnswers.push('');
+                console.log("Matrix Questions: " + question.matrixQuestions);
+                console.log("Matrix Answers: " + question.answerOptions);
+            }
+            else {
+                console.log("Error: HTML answer type generator has failed.");
+            }
+            //return htmlAnswers;
+        }
+
     }
 
 }
