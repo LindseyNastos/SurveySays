a156d1c9ff89c6731cdc5a8ed0cfa8238b38919c
Lindsey <lindseynastos@gmail.com>
2016-05-16 19:45:05 -0700

    Question type custom directive implemented

--- src/SurveySays/wwwroot/ngApp/controllers/surveyControllers/previewController.ts
+++ src/SurveySays/wwwroot/ngApp/controllers/surveyControllers/previewController.ts
@@ -1,50 +1,20 @@
 ﻿namespace SurveySays.Controllers {
 
     export class PreviewController {
+        public x = "hey chaz";
         public survey: SurveySays.Models.ISurvey;
         public questions: any = [];
         constructor(private surveyService: SurveySays.Services.SurveyService, $stateParams: ng.ui.IStateParamsService) {
             surveyService.getFullSurvey($stateParams['id']).then((data) => {
                 this.survey = data.survey;
                 this.questions = data.questions;
-                for (let q of this.questions) {
-                    this.addHtml(q);
-                };
+
+                setTimeout(() => {
+                                    
+                }, 15000);
             });
+
         }
-
-        public addHtml(question: SurveySays.Models.IQuestion) {
-            setTimeout(() => {
-                let id = (question.id).toString();
-                if (question.questionType.type == "TextBox") {
-                    let x = <HTMLElement>document.getElementById(id);
-                    x.innerHTML = '<input class="form-control" type="text" name="{{question.id}}" /><br />';
-                }
-                else if (question.questionType.type == "Multiple Choice") {
-                    for (let a of question.answerOptions) {
-                        let x = <HTMLElement>document.getElementById(id);
-                        x.innerHTML = '<div ng-repeat="answer in question.answerOptions"><input type="radio" name="{{question.id}}" value="{{answer.id}}" />{{answer.opt}}<br /></div>';
-                    }
-                }
-                else if (question.questionType.type == "Ranking") {
-                    let x = <HTMLElement>document.getElementById(id);
-                    x.innerHTML = '<div ng-repeat="answer in question.answerOptions"><input type="number" name="{{question.id}}" />{{answer.opt}}<br /></div>';
-
-                }
-                else if (question.questionType.type == "Dropdown List") {
-                    let x = <HTMLElement>document.getElementById(id);
-                    x.innerHTML = '<select class="form-control"><option value="{{answer.id}}" ng-repeat="answer in question.answerOptions">{{answer.opt}}</option></select>';
-                }
-                else if (question.questionType.type == "Matrix Rating") {
-                    console.log("Matrix Questions: " + question.matrixQuestions);
-                    console.log("Matrix Answers: " + question.answerOptions);
-                }
-                else {
-                    console.log("Error: HTML answer type generator has failed.");
-                }
-            }, 1000);
-        }
-
     }
 
 }
---
+++ src/SurveySays/wwwroot/ngApp/directives/answersByQuestionTypeDirective.ts
@@ -0,0 +1,104 @@
+﻿namespace SurveySays.Directives {
+
+    class QuestionTypeDirective implements ng.IDirective {
+        public restrict: 'E';
+        //public link: (scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => void;
+        public scope = { question: '=surveyQuestion' };
+        public link = (scope: any, element: ng.IAugmentedJQuery, attrs: ng.IAttributes, ctrl: any) => {
+            let answerTemplate = this.checkType(scope.question);
+            this.$templateRequest(answerTemplate).then((html) => {
+                // Convert template to actual html
+                let template = angular.element(html);
+                // Append it to the directive element
+                element.append(template);
+                // And let Angular $compile it
+                this.$compile(template)(scope);
+            });
+        }
+
+        constructor(public $compile: ng.ICompileService, public $templateRequest: ng.ITemplateRequestService) {
+
+        }
+
+        public static Factory() {
+            var directive = function ($compile, $templateRequest) {
+                return new QuestionTypeDirective($compile, $templateRequest);
+            };
+            return directive;
+        }
+
+        public checkType(question: SurveySays.Models.IQuestion): string {
+            let answerTemplate: string;
+            if (question.questionType.type == "TextBox") {
+                answerTemplate = '/ngApp/views/questionTemplates/textbox.html';
+            }
+            else if (question.questionType.type == "Multiple Choice") {
+                for (let a of question.answerOptions) {
+                    answerTemplate = '/ngApp/views/questionTemplates/multipleChoice.html';
+                }
+            }
+            else if (question.questionType.type == "Ranking") {
+                answerTemplate = '/ngApp/views/questionTemplates/ranking.html';
+
+            }
+            else if (question.questionType.type == "Dropdown List") {
+                answerTemplate = '/ngApp/views/questionTemplates/dropdownList.html';
+            }
+            else if (question.questionType.type == "Matrix Rating") {
+                answerTemplate = '/ngApp/views/questionTemplates/matrixRating.html';
+            }
+            else {
+                console.log("Error: HTML answer type generator has failed.");
+            }
+            return answerTemplate;
+        }
+    }
+    angular.module("SurveySays").directive('questionTypeDirective', QuestionTypeDirective.Factory());
+}
+
+    //angular.module("SurveySays").directive('questionTypeDirective', function ($compile, $templateRequest) {
+    //    function checkType(question: SurveySays.Models.IQuestion) {
+    //        let answerTemplate: string;
+    //        if (question.questionType.type == "TextBox") {
+    //            answerTemplate = '/ngApp/views/questionTemplates/textbox.html';
+    //        }
+    //        else if (question.questionType.type == "Multiple Choice") {
+    //            for (let a of question.answerOptions) {
+    //                answerTemplate = '/ngApp/views/questionTemplates/multipleChoice.html';
+    //            }
+    //        }
+    //        else if (question.questionType.type == "Ranking") {
+    //            answerTemplate = '/ngApp/views/questionTemplates/ranking.html';
+
+    //        }
+    //        else if (question.questionType.type == "Dropdown List") {
+    //            answerTemplate = '/ngApp/views/questionTemplates/dropdownList.html';
+    //        }
+    //        else if (question.questionType.type == "Matrix Rating") {
+    //            answerTemplate = '/ngApp/views/questionTemplates/matrixRating.html';
+    //        }
+    //        else {
+    //            console.log("Error: HTML answer type generator has failed.");
+    //        }
+    //        return answerTemplate;
+    //    }
+
+    //    return {
+    //        restrict: 'E',
+    //        scope: {
+    //            question: '=surveyQuestion'
+    //        },
+    //        link: function (scope: any, element, attrs) {
+    //            var answerTemplate = checkType(scope.question);
+    //            $templateRequest(answerTemplate).then(function (html) {
+    //                // Convert template to actual html
+    //                var template = angular.element(html);
+    //                // Append it to the directive element
+    //                element.append(template);
+    //                // And let Angular $compile it
+    //                $compile(template)(scope);
+    //            });
+    //        },
+    //    };
+    //});
+
