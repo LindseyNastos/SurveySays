97991a728a96aa64cd16118f20247efeb9fcd6fd
Lindsey <lindseynastos@gmail.com>
2016-05-19 17:23:06 -0700

    Datepicker popup added

--- src/SurveySays/wwwroot/ngApp/controllers/surveyControllers/collectController.ts
+++ src/SurveySays/wwwroot/ngApp/controllers/surveyControllers/collectController.ts
@@ -2,8 +2,85 @@
 
     export class CollectController {
         public courses: SurveySays.Models.ICourse[];
+        public sendTime;
+        public formats: string[] = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        public format: string = this.formats[0];
+        public dt;
+        public events;
+        public popup = { opened: false };
+        public dateOptions = {
+            dateDisabled: this.disabled,
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date(),
+            startingDay: 1
+        };
+        public inlineOptions = {
+            customClass: this.getDayClass,
+            minDate: new Date(),
+            showWeeks: true
+        };
+
+        //METHODS
+        public toggleDatePopup() {
+            let later = <HTMLInputElement>document.getElementById('datePopup');
+            if (this.sendTime == "later") {
+                later.style.display = "";
+            }
+            else {
+                later.style.display = "none";
+            }
+        };
+
+        public today() {
+            this.dt = new Date();
+        };
+
+        public clear() {
+            this.dt = null;
+        };
+
+        public open() {
+            this.popup.opened = true;
+        };
+
+        public disabled(data) {
+            let date = data.date,
+                mode = data.mode;
+            return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+        }
+
+        public toggleMin() {
+            this.inlineOptions.minDate = this.inlineOptions.minDate ? null : new Date();
+            this.dateOptions.minDate = this.inlineOptions.minDate;
+        };
+
+        public setDate(year, month, day) {
+            this.dt = new Date(year, month, day);
+        };
+
+        public getDayClass(data) {
+            let date = data.date,
+                mode = data.mode;
+            if (mode === 'day') {
+                var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+                for (let i = 0; i < this.events.length; i++) {
+                    let currentDay = new Date(this.events[i].date).setHours(0, 0, 0, 0);
+
+                    if (dayToCheck === currentDay) {
+                        return this.events[i].status;
+                    }
+                }
+            }
+            return '';
+        }
+
+        //CTOR
         constructor(private courseService: SurveySays.Services.CourseService) {
             this.courses = courseService.listCourses();
+            this.today();
+            this.toggleMin();
         }
+        
     }
 }
