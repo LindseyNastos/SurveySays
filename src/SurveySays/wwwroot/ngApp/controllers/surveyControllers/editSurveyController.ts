4080a25d5a4a8ac6b52680614554db8082701f15
Lindsey <lindseynastos@gmail.com>
2016-05-20 19:28:21 -0700

    Tab routing added

--- src/SurveySays/wwwroot/ngApp/controllers/surveyControllers/editSurveyController.ts
+++ src/SurveySays/wwwroot/ngApp/controllers/surveyControllers/editSurveyController.ts
@@ -2,12 +2,20 @@
 
     export class EditSurveyController {
         public survey: SurveySays.Models.ISurvey;
+        public isActive: boolean;
         constructor(private surveyService: SurveySays.Services.SurveyService, $stateParams: ng.ui.IStateParamsService) {
             this.survey = this.surveyService.getBasicSurvey($stateParams['id']);
+            this.removeActive();
+
         }
         public saveChanges(survey) {
             this.surveyService.saveSurvey(survey);
         }
+        public removeActive() {
+            setTimeout(() => {
+                let defaultTab = <HTMLLIElement>document.getElementById('analyze-tab');
+                defaultTab.classList.remove("active");
+            }, 1);
+        }
     }
-
 }
